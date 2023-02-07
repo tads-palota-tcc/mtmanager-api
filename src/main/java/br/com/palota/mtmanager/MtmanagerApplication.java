69108a724ca64d3f333d5aa2062663586d20568c
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2023-02-07 09:43:11 -0300

    feat: project created

---
+++ src/main/java/br/com/palota/mtmanager/MtmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.palota.mtmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtmanagerApplication.class, args);
+	}
+
+}
