ef739b17d05770443aabd9c9e2a0e8dd27d6ee9d
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2023-02-10 17:32:54 -0300

    feat: error handling

--- src/main/java/br/com/palota/mtmanager/api/dto/AddressCreationDTO.java
+++ src/main/java/br/com/palota/mtmanager/api/dto/AddressCreationDTO.java
@@ -3,15 +3,19 @@ package br.com.palota.mtmanager.api.dto;
 import br.com.palota.mtmanager.domain.model.State;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AddressCreationDTO {
 
     @NotBlank
+    @Size(min = 3, max = 100)
     private String streetName;
 
     @NotBlank
+    @Size(min = 1, max = 10)
     private String number;
 
     private String complement;
@@ -19,12 +23,14 @@ public class AddressCreationDTO {
     private String neighborhood;
 
     @NotBlank
+    @Size(min = 2, max = 100)
     private String city;
 
     @NotNull
     private State state;
 
     @NotBlank
+    @Pattern(regexp = "\\d{8}")
     private String zipCode;
 
 }
--- src/main/java/br/com/palota/mtmanager/api/dto/PlantCreationDTO.java
+++ src/main/java/br/com/palota/mtmanager/api/dto/PlantCreationDTO.java
@@ -2,17 +2,22 @@ package br.com.palota.mtmanager.api.dto;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class PlantCreationDTO {
 
     @NotBlank
+    @Size(min = 1, max = 20)
     private String code;
 
     @NotBlank
+    @Size(min = 3, max = 60)
     private String name;
 
+    @NotNull
     @Valid
     private AddressCreationDTO address;
 }
