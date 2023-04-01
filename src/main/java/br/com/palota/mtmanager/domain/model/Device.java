10cd1f420ebd5bc30ac2fe1195e6f6db0d49dc89
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2023-04-01 19:26:58 -0300

    fix: optimize imports

--- src/main/java/br/com/palota/mtmanager/domain/model/Device.java
+++ src/main/java/br/com/palota/mtmanager/domain/model/Device.java
@@ -1,6 +1,5 @@
 package br.com.palota.mtmanager.domain.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
